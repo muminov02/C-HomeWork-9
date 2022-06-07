a99eca4e0f934b39c99956339acfda97f7d494d7
Umid <spec7575@mail.ru>
2022-06-07 12:25:30 +0500

    HomeWork

---
+++ task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿void FindEven(int m, int n)
+{
+    if(m > n) return;
+    if(m%2 == 0) Console.Write($"{m}, ");
+    FindEven(m+1,n);
+}
+
+FindEven(4,8);
---
+++ task_2/Program.cs
@@ -0,0 +1,7 @@
+﻿int FindSum(int m, int n)
+{
+    if (m > n) return 0;
+    return FindSum(m + 1, n) + m;
+}
+
+Console.WriteLine(FindSum(4,8));
